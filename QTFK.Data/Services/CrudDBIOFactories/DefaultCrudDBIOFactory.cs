726cc0dd59f71336b75a9fcc49c3045c805d40b2
hossmi <hossmi@gmail.com>
2017-05-10 00:32:23 +0200

    Added ICrudDBIOFactory and its default implementation

---
+++ QTFK.Data/Services/CrudDBIOFactories/DefaultCrudDBIOFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services.CrudDBIOFactories
+{
+    public class DefaultCrudDBIOFactory : ICrudDBIOFactory
+    {
+        public ICRUDDBIO Get<T>() where T : ICRUDDBIO, IDBIO
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICRUDDBIO GetDefault()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QTFK.Data/Services/ICrudDBIOFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services
+{
+    public interface ICrudDBIOFactory
+    {
+        ICRUDDBIO GetDefault();
+        ICRUDDBIO Get<T>() where T : ICRUDDBIO, IDBIO;
+    }
+}
