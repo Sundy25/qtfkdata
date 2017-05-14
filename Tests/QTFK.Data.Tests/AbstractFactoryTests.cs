93f8cd69c695bd6274c7c8da5d071ce73904e289
hossmi <hossmi@gmail.com>
2017-05-14 21:40:16 +0200

    Added AbstractFactory Test (Pure TDD) :D

---
+++ Tests/QTFK.Data.Tests/AbstractFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QTFK.Services;
+using QTFK.Services.Factories;
+using QTFK.Services.DBIO;
+using System.Collections.Generic;
+
+namespace QTFK.Data.Tests
+{
+    public class SampleClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal WalletCash { get; set; }
+    }
+
+    public interface ISampleClassRepository : ICRUDRepository<int, SampleClass>
+    {
+        IEnumerable<SampleClass> GetByWalletCash(decimal walletCash);
+        IEnumerable<SampleClass> GetWithWalletCashBetween(decimal minWalletCash, decimal maxWalletCash);
+    }
+
+    [TestClass]
+    public class AbstractFactoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IAbstractEntityFactory superFactory = new DefaultAbstractEntityFactory(
+                new SqlServerCrudFactory(
+                    new SQLServerDBIO("")
+                    ));
+
+            ISampleClassRepository repository = superFactory.Get<ISampleClassRepository>();
+
+            var samples = repository.GetWithWalletCashBetween(-1000m, 100000m);
+        }
+    }
+}
