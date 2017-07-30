b6deae706a169c67896c3843877294921d2d4ce1
Hossmi <hossmi@gmail.com>
2017-07-30 20:56:25 +0200

    - Added QTFK.Data.OleDB project for filter implementations for OleDB

---
+++ QTFK.Data.OleDB/Services/DBIO/QueryFilterFactories/OleDBDefaultFilterFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using QTFK.Models;
+
+namespace QTFK.Services.DBIO.QueryFilterFactories
+{
+    public class OleDBDefaultFilterFactory : IQueryFilterFactory
+    {
+        public IQueryFilter Build(MethodBase method, Type type)
+        {
+            return null;
+        }
+    }
+}
