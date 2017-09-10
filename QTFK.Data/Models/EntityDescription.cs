d473a207b11fcd6814fe0d8fcda0385d208d293b
Hossmi <hossmi@gmail.com>
2017-09-10 14:35:58 +0200

    Class for Type description

---
+++ QTFK.Data/Models/EntityDescription.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace QTFK.Models
+{
+    public class EntityDescription
+    {
+        public EntityDescription(string name, IEnumerable<string> fields)
+        {
+            Name = name;
+            Fields = fields;
+        }
+
+        public string Name { get; }
+        public IEnumerable<string> Fields { get; }
+    }
+}
