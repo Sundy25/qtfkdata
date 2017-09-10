64614f48db5c465f70c42354af9ca0ad60368be5
Hossmi <hossmi@gmail.com>
2017-09-10 19:57:03 +0200

    Added Id field name and PropertyInfo for ID on EntityDescription class

--- QTFK.Data/Models/EntityDescription.cs
+++ QTFK.Data/Models/EntityDescription.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 
 namespace QTFK.Models
 {
     public class EntityDescription
     {
-        public EntityDescription(string name, IEnumerable<string> fields)
+        public EntityDescription(string id, PropertyInfo keyProp, string name, IEnumerable<string> fields)
         {
             Name = name;
             Fields = fields;
+            Id = id;
+            PropertyId = keyProp;
         }
 
         public string Name { get; }
         public IEnumerable<string> Fields { get; }
+        public string Id { get; set; }
+        public PropertyInfo PropertyId { get; }
     }
 }
