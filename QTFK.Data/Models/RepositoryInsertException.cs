b213c00f0019a499a1b5063610645eec582705a8
Hossmi <hossmi@gmail.com>
2017-09-10 19:54:27 +0200

    Renamed attributes Added Insert exception

---
+++ QTFK.Data/Models/RepositoryInsertException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace QTFK.Models
+{
+    [Serializable]
+    public class RepositoryInsertException : Exception
+    {
+        public RepositoryInsertException()
+        {
+        }
+
+        public RepositoryInsertException(IDBQueryInsert q)
+        {
+            Query = q;
+        }
+
+        public RepositoryInsertException(string message) : base(message)
+        {
+        }
+
+        public RepositoryInsertException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RepositoryInsertException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public IDBQueryInsert Query { get; }
+    }
+}
