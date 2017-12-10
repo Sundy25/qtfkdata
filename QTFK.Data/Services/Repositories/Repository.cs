cd5d3673074652cdfe493b28c4a61088ce1cbb4d
Hossmi <hossmi@gmail.com>
2017-12-10 12:00:30 +0100

    Added repository test (forgotten files)

---
+++ QTFK.Data/Services/Repositories/Repository.cs
@@ -0,0 +1,14 @@
+﻿namespace QTFK.Services.Repositories
+{
+    public class Repository<T> : BaseRepository<T> where T : class, new()
+    {
+        public Repository() 
+            : base()
+        {
+        }
+        public Repository(IDBIO db, IQueryFactory queryFactory) 
+            : base(db, queryFactory)
+        {
+        }
+    }
+}
