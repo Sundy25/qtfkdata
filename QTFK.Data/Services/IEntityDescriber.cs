bc70c5395a29233583456b3b528ae176705ad8c9
Hossmi <hossmi@gmail.com>
2017-12-10 21:40:14 +0100

    Added IEntityDescriber for to refactor IEntityQueryFactory

---
+++ QTFK.Data/Services/IEntityDescriber.cs
@@ -0,0 +1,17 @@
+﻿using QTFK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services
+{
+    public interface IEntityDescriber
+    {
+        EntityDescription getDescription(Type entityType);
+        //string getName(Type entity);
+        //IEnumerable<string> getFields(Type entity);
+        //IEnumerable<string> getId(Type entity);
+    }
+}
