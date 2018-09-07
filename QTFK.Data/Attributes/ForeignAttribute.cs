b89342c28e55357efab0c728757c3a06a49d7e64
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2018-09-07 12:38:24 +0200

    Added ForeignAttribute for one to many relations

---
+++ QTFK.Data/Attributes/ForeignAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QTFK.Attributes
+{
+    public class ForeignAttribute : Attribute
+    {
+        public ForeignAttribute(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+
+        public string ColumnName { get; }
+    }
+}
