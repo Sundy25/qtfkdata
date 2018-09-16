a109c142b5710207612185f62a63d14698b4b04f
Hossmi <hossmi@gmail.com>
2018-09-16 21:28:52 +0200

    Added second test class for DbMetaDataBuilder tests

---
+++ Tests/QTFK.Data.Tests/UnitTest2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QTFK.Data.Tests.Services;
+using QTFK.Services.DbFactory;
+
+namespace QTFK.Data.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDbMetadata dbMetadata;
+            IMetadataBuilder metadataBuilder;
+
+            metadataBuilder = new DefaultMetadataBuilder();
+            dbMetadata = metadataBuilder.scan<IExpensesDB>();
+
+            Assert.IsNotNull(dbMetadata);
+            Assert.AreEqual(typeof(IExpensesDB), dbMetadata.InterfaceType);
+        }
+    }
+}
