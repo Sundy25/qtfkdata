318685b739b8c4c1a6abb47f6790d9f33e950bd9
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2018-09-18 13:32:55 +0200

    Added IColumnMetaData

---
+++ QTFK.Data/Services/DbFactory/IColumnMetaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QTFK.Services.DbFactory
+{
+    public interface IColumnMetaData
+    {
+        string Name { get; }
+        string ColumnName { get; }
+        Type ColumnType { get; }
+        bool IsPrimaryKey { get; }
+        bool IsForeignKey { get; }
+        bool IsAlternativeKey { get; }
+    }
+}
