5e1abae189b5d1f2bf6d9203dde59bb1fd0e2a28
Hossmi <hossmi@gmail.com>
2018-09-30 21:18:12 +0200

    feat: half SqlServer prototype implementation of IReadonlyUsersDB

--- Tests/SimpleDB1/DataBases/Sample1/Prototypes/SqlServer/UserDBSqlServerPrototype.cs
+++ Tests/SimpleDB1/DataBases/Sample1/Prototypes/SqlServer/UserDBSqlServerPrototype.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using QTFK.Services;
+using QTFK.Services.DBIO;
 
-namespace SimpleDB1.DataBases.Sample1.Prototypes.InMemory
+namespace SimpleDB1.DataBases.Sample1.Prototypes.SqlServer
 {
-    public class UserDBInMemoryPrototype : IReadonlyUsersDB
+    public class UserDBSqlServerPrototype : IReadonlyUsersDB
     {
         private class PrvEngineFeatures : IEngineFeatures
         {
             public PrvEngineFeatures()
             {
-                this.SupportsStoredProcedures = false;
-                this.SupportsTransactions = false;
+                this.SupportsStoredProcedures = true;
+                this.SupportsTransactions = true;
             }
 
             public bool SupportsTransactions { get; }
@@ -47,61 +47,71 @@ namespace SimpleDB1.DataBases.Sample1.Prototypes.InMemory
             }
         }
 
+        private class PrvUser : IUser
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime BirthDate { get; set; }
+            public bool IsEnabled { get; set; }
+        }
+
         private class PrvUsersView : IView<IUser>
         {
-            private readonly IList<IUser> users;
+            private readonly ISqlServerDBIO dbio;
 
-            public PrvUsersView()
+            public PrvUsersView(ISqlServerDBIO dbio)
             {
-                this.users = new List<IUser>();
+                this.dbio = dbio;
             }
 
-            public int Count
-            {
-                get
-                {
-                    return this.users.Count;
-                }
-            }
+            public int Count { get; }
 
             public IEnumerator<IUser> GetEnumerator()
             {
-                return this.users.GetEnumerator();
+                throw new NotImplementedException();
             }
 
             public IPageView<IUser> paginate(int pageSize, int page)
             {
-                int pagesCount, offset;
                 IEnumerable<IUser> paginatedUsers;
+                int pagesCount;
 
-                offset = pageSize * page;
-                pagesCount = (int)Math.Ceiling((decimal)this.users.Count / pageSize);
-                paginatedUsers = this.users
-                    .Skip(offset)
-                    .Take(pageSize);
+                throw new NotImplementedException();
 
                 return new PrvPageView<IUser>(pageSize, page, pagesCount, paginatedUsers);
             }
 
             IEnumerator IEnumerable.GetEnumerator()
             {
-                return this.users.GetEnumerator();
+                throw new NotImplementedException();
             }
-
         }
 
-        public UserDBInMemoryPrototype()
+        private readonly ISqlServerDBIO dbio;
+
+        public UserDBSqlServerPrototype(ISqlServerDBIO dbio)
         {
             this.EngineFeatures = new PrvEngineFeatures();
-            this.Users = new PrvUsersView();
+            this.dbio = dbio;
+            this.Users = new PrvUsersView(dbio);
         }
 
-        public IEngineFeatures EngineFeatures { get; }
         public IView<IUser> Users { get; }
+        public IEngineFeatures EngineFeatures { get; }
 
         public void transact(Func<bool> transactionBlock)
         {
-            throw new NotSupportedException();
+            this.dbio.Set(command =>
+            {
+                bool blockResult;
+
+                blockResult = transactionBlock();
+
+                if (blockResult == false)
+                    command.Transaction.Rollback();
+
+                return 0;
+            });
         }
 
     }
