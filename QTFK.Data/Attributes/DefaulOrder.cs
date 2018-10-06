ff0db69c7afc7cc5f11bca73c5ec7c45b01fee9c
Hossmi <hossmi@gmail.com>
2018-10-06 21:57:38 +0200

    feat: added DefaultOrderAttibute

---
+++ QTFK.Data/Attributes/DefaulOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QTFK.Attributes
+{
+    public enum ColumnOrder
+    {
+        Ascendant,
+        Descendant,
+    }
+
+    public class DefaulOrderAttribute : Attribute
+    {
+        public DefaulOrderAttribute() : this(ColumnOrder.Ascendant)
+        {
+        }
+
+        public DefaulOrderAttribute(ColumnOrder order)
+        {
+            this.Order = order;
+        }
+
+        public ColumnOrder Order { get; }
+    }
+}
